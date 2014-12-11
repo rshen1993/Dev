e07684b4e79f0e26833833268ec42331a0185b97
xuxiaoyu89 <xuxiaoyu89@gmail.com>
2014-12-11 16:45:08 -0500

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -491,8 +491,22 @@ function ($sce, $scope, $rootScope, $log, $window, $timeout, $location, $interva
     alert("sendMessageToPhonegap:" + message);
     window.parent.postMessage(message, "*");
    }
+   
+   function makeAjaxCall(url, callback){
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+            var responseText = xmlhttp.responseText;
+            console.log(responseText);
+            callback(responseText);
+        }
+        };
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+    }
     
-    makeAjaxCall("phoneGap.js",
+    
+    makeAjaxCall("phoneGapCode.js",
     function(responseText) {
       alert('Loaded phonegapIframe_phonegapCode.js: ' + responseText);
       sendMessageToPhonegap(responseText);
